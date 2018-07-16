21475700bddb99356ebaaef10c4a554af05bd2df
Evert Pot <me@evertpot.com>
2018-07-16 10:20:37 -0700

    Better design

--- js/webmentions.js
+++ js/webmentions.js
@@ -20,8 +20,6 @@ function loadWebMentions() {
   // fix dev url
   url = url.replace(/^http:\/\/localhost:4000/, 'https://evertpot.com');
 
-
-
   return fetch('https://webmention.io/api/mentions?target=' + url)
     .then( response => response.json() )
     .then( result => displayWebMentions(elem, result) );
@@ -37,13 +35,20 @@ function displayWebMentions(elem, result) {
 
   var activityHtml = [];
   var likeHtml = [];
+  var repostHtml = [];
   for(var linkIdx in result.links) {
     var link = result.links[linkIdx];
 
     switch(link.activity.type) {
       case 'like' :
         likeHtml.push(getAvatar(link, link.data.url));
-        break
+        break;
+      case 'repost' :
+        repostHtml.push(getAvatar(link, link.data.url));
+        break;
+      case 'reply' :
+        activityHtml.push('<li><div class="comment">' + getAvatar(link) + '<div class="inner"><p>' + getAuthorName(link) + ' • <time>' + getPublishedTime(link) + '</time></p>' + link.data.content + '</div></div>');
+        break;
       default :
         if (link.activity.sentence_html) {
           activityHtml.push('<li>' + getAvatar(link) + ' <p>' + link.activity.sentence_html + '</p></li>');
@@ -55,11 +60,14 @@ function displayWebMentions(elem, result) {
 
   var html = '';
 
-  if (activityHtml.length) {
-    html += '<ul class="activity">' + activityHtml.join('\n') + '</ul>\n';
+  if (repostHtml.length) {
+    html += '<div class="facepile"><h2>Reposts:</h2>' + repostHtml.join('\n') + '</div>\n';
   }
   if (likeHtml.length) {
-    html += '<div class="likes"><h2>Likes:</h2>' + likeHtml.join('\n') + '</p>\n';
+    html += '<div class="facepile"><h2>Likes:</h2>' + likeHtml.join('\n') + '</div>\n';
+  }
+  if (activityHtml.length) {
+    html += '<ul class="activity">' + activityHtml.join('\n') + '</ul>\n';
   }
 
   elem.innerHTML = html;
@@ -74,6 +82,36 @@ function getAvatar(link, url) {
   if (link.data && link.data.author && link.data.author.photo) {
     return '<a href="' + h(url) + '" title="' + h(link.data.author.name) + '"><img src="' + h(link.data.author.photo) + '" alt="' + h(link.data.author.name) + '" /></a>';
   }
+
+}
+function getAuthorName(link, url) {
+
+  url = link.data.author.url;
+
+  if (link.data && link.data.author) {
+    return '<a href="' + h(url) + '" title="' + h(link.data.author.name) + '">' + h(link.data.author.name) + '</a>';
+  }
+
+}
+function getPublishedTime(link) {
+
+  var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  var d = new Date(link.data.published_ts * 1000);
+  return months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear();
 
 }
 
