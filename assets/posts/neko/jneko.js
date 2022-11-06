789e0f23af8246c7b8bf1d3bd94dc239bca5d80e
Evert Pot <me@evertpot.com>
2022-11-06 15:35:14 -0500

    Dont preload images we dont use

--- assets/posts/neko/jneko.js
+++ assets/posts/neko/jneko.js
@@ -2,35 +2,35 @@
 
 const imageNames = [
   'awake',
-  'down1',
-  'down2',
-  'dtogi1',
-  'dtogi2',
-  'dwleft1',
-  'dwleft2',
+//  'down1',
+//  'down2',
+//  'dtogi1',
+//  'dtogi2',
+//  'dwleft1',
+//  'dwleft2',
   'jare2',
   'kaki1',
   'kaki2',
-  'left1',
-  'left2',
-  'ltogi1',
-  'ltog2',
+//  'left1',
+//  'left2',
+//  'ltogi1',
+//  'ltog2',
   'mati2',
   'mati3',
-  'right1',
-  'right2',
-  'rtogi1',
-  'rtogi2',
+//  'right1',
+//  'right2',
+//  'rtogi1',
+//  'rtogi2',
   'sleep1',
   'sleep2',
-  'up1',
-  'up2',
-  'upleft1',
-  'upleft2',
-  'upright1',
-  'upright2',
-  'utogi1',
-  'utogi2',
+//  'up1',
+//  'up2',
+//  'upleft1',
+//  'upleft2',
+//  'upright1',
+//  'upright2',
+//  'utogi1',
+//  'utogi2',
 ];
 const nekoSize = 64; 
 const images = Object.fromEntries(imageNames.map(name => {
@@ -77,8 +77,6 @@ const stateMachine = {
 };
 
 class Neko {
-
-
 
   constructor(elem) {
 
