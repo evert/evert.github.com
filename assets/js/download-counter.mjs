900f20383b156a3f1574d93243f66d5484eba703
Evert Pot <me@evertpot.com>
2024-06-29 18:28:08 -0400

    Download counter

---
+++ assets/js/download-counter.mjs
@@ -0,0 +1,37 @@
+class DownloadCounter extends HTMLSpanElement {
+  static observedAttributes = [
+    'date',
+    'inc-per-day'
+  ];
+
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.count = +this.textContent;
+    this.date = new Date(this.getAttribute('date'));
+    this.inc = (+this.getAttribute('inc-per-day')) / (3600 * 24 * 1000) 
+    this.calculateCurrentDownloads();
+
+  }
+
+  calculateCurrentDownloads() {
+
+    const currentDownloads =
+      Math.floor(
+      this.count +
+      (Date.now()-this.date) * this.inc
+      );
+
+    this.textContent = currentDownloads;
+
+    setTimeout(
+      () => this.calculateCurrentDownloads(),
+      Math.floor(Math.random() * 150)+50,
+    );
+
+  }
+
+}
+
+customElements.define('download-counter', DownloadCounter, { extends: 'span' });
